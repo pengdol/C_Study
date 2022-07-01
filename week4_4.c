bf60b3f5758be54b350c176807638f51dd58cd5e
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2022-07-01 17:47:46 +0900

    Create week4_4.c
    baekjoon #10869

---
+++ week4_4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+  int num1;
+  int num2;
+  
+  scanf("%d %d", &num1, &num2);
+  printf("%d\n%d\n%d\n%d\n%d", num1 + num2, num1 - num2, num1 * num2, num1 / num2, num1 % num2);
+  
+  //baekjoon #10869
+}
