b534b44757c30ec3887cb3c81f9bd60c1aaa3da3
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2022-07-15 15:22:28 +0900

    Create week5_2.c

---
+++ week5_2.c
@@ -0,0 +1,24 @@
+void main()
+{
+  int res;
+
+  res = 0 && 0;
+  printf("%d\n", res);
+  res = 0 && 1;
+  printf("%d\n", res);
+  res = 1 && 0;
+  printf("%d\n", res);
+  res = 1 && 1;
+  printf("%d\n", res);
+
+  res = 0 || 0;
+  printf("%d\n", res);
+  res = 0 || 1;
+  printf("%d\n", res);
+  res = 1 || 0;
+  printf("%d\n", res);
+  res = 1 || 1;
+  printf("%d\n", res);
+
+  printf("%d\n", !res);
+}
