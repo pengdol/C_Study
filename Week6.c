e4131b9346926bc77554110301c636a66568b4ad
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2022-07-15 16:42:25 +0900

    Create Week6.c

---
+++ Week6.c
@@ -0,0 +1,44 @@
+/*void main()
+{
+  int a, b;
+
+  a = 20;
+  b = 5;
+
+  a += b;
+  printf("a = %d\n", a);
+  a *= b;
+  printf("a = %d\n", a);
+  a -= b;
+  printf("a = %d\n", a);
+  a /= b;
+  printf("a = %d\n", a);
+  a %= b;
+  printf("a = %d\n", a);
+}*/
+
+/*void main()
+{
+  int a = 10;
+
+  a++;
+  a++;
+  printf("a = %d\n", a);
+  ++a;
+  ++a;
+  printf("a = %d\n", a);
+}*/
+
+/*void main()
+{
+  int a = 20;
+
+  a += 4;
+  printf("a = %d\n", a);
+  a /= 2;
+  printf("a = %d\n", a);
+  a -= 1;
+  printf("a = %d\n", a);
+  a *= 11;
+  printf("a = %d\n", a);
+}*/
