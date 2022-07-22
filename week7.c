9787a9b210291b21c7064f1d35fd9abc65e2ae49
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2022-07-22 16:01:28 +0900

    Create week7.c

---
+++ week7.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/*void main()
+{
+  int big = 5730;
+  int small = 5730;
+  printf("%d", big-small);
+}*/
+
+/*void main()
+{
+  int x = 100;
+  printf("%d\n", ((x+10+10+10)/2/2)-5-5);
+
+  int a = 50;
+  printf("%d", ((a*10+10)*10-5-5)/2);
+}*/
+
+/*void main()
+{
+  int a;
+  scanf("%d", &a);
+  printf("%d분 %d초",  a / 60, a % 60);
+}*/
+
+/*void main()
+{
+  int w;
+  float h;
+  scanf("%d %f", &w, &h);
+  w = w - 5; h = h + 0.1;
+  printf("키: %0.2fm, 몸무게: %dkg, 체질량 지수: %f", h, w, w/h*h);
+}*/
+
+/*void main()
+{
+  int n = 0;
+  printf("%d\n", n);
+  printf("%d\n", n += 3);
+  printf("%d\n", n *= 0);
+  printf("%d\n", n += 5);
+  printf("%d\n", n++);
+  printf("%d\n", ++n);
+  printf("%d\n", n % 5);
+  printf("%d\n", --n);
+  printf("%d\n", n--);
+  printf("%d\n", n);
+}*/
