989700bbb807e6066fd307d7b73e8c91bda86708
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2022-07-22 16:56:49 +0900

    Update week7.c

--- week7.c
+++ week7.c
@@ -63,3 +63,30 @@
   else
     printf("공약수 X\n");
 }*/
+
+/*void main()
+{
+  float a, b, c;
+  scanf("%f %f %f", &a, &b, &c);
+  if(a>b && a>c)
+  {
+    if(b>c)
+      printf("%f %f %f", a, b, c);
+    else
+      printf("%f %f %f", a, c, b);
+  }
+  else if(b>a && b>c)
+  {
+    if(a>c)
+      printf("%f %f %f", b, a, c);
+    else
+      printf("%f %f %f", b, c, a);
+  }
+  else
+  {
+    if(a>b)
+      printf("%f %f, %f", c, a, b);
+    else
+      printf("%f %f %f", c, b, a);
+  }
+}*/
