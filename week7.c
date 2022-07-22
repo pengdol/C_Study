9057a6e0b3d0cc91111084ebb0c8b86b29f57e2a
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2022-07-22 17:52:44 +0900

    Update week7.c

--- week7.c
+++ week7.c
@@ -90,3 +90,26 @@
       printf("%f %f %f", c, b, a);
   }
 }*/
+/*void main()
+{
+  int n;
+
+  for (n=30; n > 20; n--)
+    {
+      printf("n = %d\n", n); 
+    }
+
+  printf("마지막 n = %d\n", n);
+}*/
+
+/*void main()
+{
+  int n;
+
+  for (n=-10; n <= -1; n++)
+    {
+      printf("n = %d\n", n); 
+    }
+
+  printf("마지막 n = %d\n", n);
+}*/
