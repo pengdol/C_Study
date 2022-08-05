3546d1bde4f94b99dcf05c4b0a92f92b27488875
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2022-08-05 16:07:27 +0900

    Create week8.c

---
+++ week8.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+
+/*void main()
+{
+  int a; scanf("%d", &a);
+  if(a>0)
+    printf("양수");
+  else if(a == 0)
+    printf("둘다 아님");
+  else
+    printf("음수");
+}*/
+
+/*void main()
+{
+  int a; scanf("%d", &a);
+  if(a%2==0)
+    printf("짝수");
+  else if(a == 0)
+    printf("짝수");
+  else
+    printf("홀수");
+}*/
+
+/*void main()
+{
+  int a; scanf("%d", &a);
+  if(a%3==0)
+    printf("3의 배수");
+  else
+    printf("3의 배수 아님");
+}*/
+
+/*void main()
+{
+  int a; int b; scanf("%d %d", &a, &b);
+  if(a>b)
+    if(a%b==0)
+      printf("나누어 떨어진다");
+    else
+      printf("안 나누어 떨어진다");
+  else
+    if(b%a==0)
+      printf("나누어 떨어진다");
+    else
+      printf("안 나누어 떨어진다");
+}*/
+
+/*void main()
+{
+  float a; float b; float c; float d; float e;
+  scanf("%f %f %f %f %f", &a, &b, &c, &d, &e);
+  if(a>b&&a>c&&a>d&&a>e)
+    printf("%f", a);
+  else if(b>a&&b>c&&b>d&&b>e)
+    printf("%f", b);
+  else if(c>b&&c>a&&c>d&&c>e)
+    printf("%f", c);
+  else if(d>b&&d>c&&d>a&&d>e)
+    printf("%f", d);
+  else if(e>b&&e>c&&e>d&&e>a)
+    printf("%f", e);*/
+
+/*void main()
+{
+  int seat; scanf("%d", &seat);
+  if(seat == 2)
+    printf("스포츠카");
+  else if(seat == 5)
+    printf("승용차");
+  else if(seat == 7)
+    printf("SUV");
+  else if(seat == 9)
+    printf("승합차");
+  else if(seat == 12)
+    printf("미니 버스");
+  else
+    printf("입력 오류");
+}*/
+
+/*void main()
+{
+  int a; scanf("%d", &a);
+  if(a%2==0&&a%3==0)
+    printf("2와 3의 공배수");
+  else
+    printf("2와 3의 공배수 X");
+}*/
+
+/*void main()
+{
+  int a; scanf("%d", &a);
+  if(12%a==0%%30%a==0)
+    printf("12와 30의 공약수");
+  else
+    printf("12와 30의 공약수 X");
+}*/
+
+/*void main()
+{
+  int a; int b; int c; scanf("%d %d %d", &a, &b, &c);
+  if(a>b&&a>c)
+    if(b>c)
+      printf("큰수: %d, 작은 수: %d", a, c);
+    else
+      printf("큰수: %d, 작은 수: %d", a, b);
+  if(b>a&&b>c)
+    if(a>c)
+      printf("큰수: %d, 작은 수: %d", b, c);
+    else
+      printf("큰수: %d, 작은 수: %d", b, a);
+  if(c>b&&c>a)
+    if(b>a)
+      printf("큰수: %d, 작은 수: %d", c, a);
+    else
+      printf("큰수: %d, 작은 수: %d", c, b);
+}*/
+
+/*void main()
+{
+  float a; float b; float c; scanf("%f %f %f", &a, &b, &c);
+  if(a>b&&a>c)
+    if(b>c)
+      printf("%f %f %f", a, b, c);
+    else
+      printf("%f %f %f", a, c, b);
+  if(b>a&&b>c)
+    if(a>c)
+      printf("%f %f %f", b, a, c);
+    else
+      printf("%f %f %f", b, c, a);
+  if(c>b&&c>a)
+    if(b>a)
+      printf("%f %f %f", c, b, a);
+    else
+      printf("%f %f %f", c, a, b);
+}*/
+
+/*void main()
+{
+  int hgt; scanf("%d", &hgt);
+  if(hgt<12)
+    printf("대기권: 대류권, 특징: 기상현상");
+  if(hgt>11&&hgt<51)
+    printf("대기권: 성층권, 특징: 오존층");
+  if(hgt>50&&hgt<81)
+    printf("대기권: 중간권, 특징: 유성관찰");
+  if(hgt>80&&hgt<1001)
+    printf("대기권: 열권, 특징: 오로라, 인공위성의 궤도");
+  if(hgt>1000)
+    printf("대기권: 외기권, 특징: 우주");
+}*/
