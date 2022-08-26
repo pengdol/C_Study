87db4639dfeba176d1f789e8fe913c455a5e8766
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2022-08-26 15:27:50 +0900

    Create Week11.c

---
+++ Week11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*void main()
+{
+  int a;
+  scanf("%d", &a);
+  switch(a%2)
+  {
+    case 0:
+      printf("짝수");
+      break;
+    default:
+      printf("홀수");
+      break;
+  }
+}*/
