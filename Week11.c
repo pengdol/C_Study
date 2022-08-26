5ad755585d09bcdd1622248b6cfab14c5a7e6061
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2022-08-26 17:15:24 +0900

    Update Week11.c

--- Week11.c
+++ Week11.c
@@ -82,3 +82,42 @@
     printf("\n");
   }
 }*/
+
+/*void main()
+{
+  int i, j;
+  for(i = 5; i >= 1; i--)
+  {
+    for(j = 1; j <= i; j++)
+    {
+      printf("%d", j);
+    }
+    printf("\n");
+  }
+}*/
+
+/*void main()
+{
+  int i, j;
+  for(i = 5; i >= 1; i--)
+  {
+    for(j = i; j <= 5; j++)
+    {
+      printf("%d", j);
+    }
+    printf("\n");
+  }
+}*/
+
+/*void main()
+{
+  int i, j;
+  for(i = 5; i >= 1; i--)
+  {
+    for(j = i; j >= 1; j--)
+    {
+      printf("%d", j);
+    }
+    printf("\n");
+  }
+}*/
