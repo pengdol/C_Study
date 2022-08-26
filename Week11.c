6800e585b47cc0510ce7b927f64e19a54e18fada
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2022-08-26 16:52:34 +0900

    Update Week11.c

--- Week11.c
+++ Week11.c
@@ -42,3 +42,16 @@
   }
 }*/
 
+/*void main()
+{
+  int i, j, k;
+  for(i = 0; i<3; i++){
+        for(j = 2; j > i; j--){
+            printf(" ");
+        }
+        for(k = 0; k<2*i+1;k++) {
+        printf("*");
+        }
+        printf("\n");
+    }
+}*/
